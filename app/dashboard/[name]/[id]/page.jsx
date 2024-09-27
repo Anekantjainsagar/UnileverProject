5660342fa56039e0dc8ecbe94e62ae4c52744c68
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-09-27 13:18:48 +0530

    Final updated

--- app/dashboard/[name]/[id]/page.jsx
+++ app/dashboard/[name]/[id]/page.jsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 import React, { useState } from "react";
 import Topbar from "../../../Components/Topbar";
 import Leftbar from "../../../Components/Leftbar";
+import { CiEdit } from "react-icons/ci";
+import { MdCheck } from "react-icons/md";
 
 const Dashboard = ({ params }) => {
-  const [value, setValue] = useState(5);
   const { id } = params;
 
   return (
@@ -46,72 +47,96 @@ const Dashboard = ({ params }) => {
               {Array(20)
                 .fill()
                 ?.map((e, i) => {
-                  return (
-                    <div
-                      key={i}
-                      className="grid border border-gray-300/30 mb-3 pl-6 pr-4 py-1 rounded-lg items-center"
-                      style={{ gridTemplateColumns: "5% 22% 21% 20% 20% 12%" }}
-                    >
-                      <p className="text-center">{id}</p>
-                      <div className="flex items-center justify-center">
-                        <Image
-                          src="/qr.png"
-                          className="w-6/12"
-                          alt="QR code"
-                          width={1000}
-                          height={1000}
-                        />
-                      </div>
-                      <div className="flex items-center justify-center">
-                        <label htmlFor="qrValue" className="mr-3">
-                          Value:
-                        </label>
-                        <input
-                          type="text"
-                          id="qrValue"
-                          value={value}
-                          onChange={(e) => setValue(e.target.value)}
-                          placeholder="Enter QR Code Value"
-                          className="outline-none border border-gray-400 px-4 rounded-lg py-0.5 text-lg w-3/12"
-                        />
-                      </div>
-                      <p className="text-center">A-102-102</p>
-                      <p className="px-4 text-lg text-center">
-                        <span className="font-medium mr-1">Path:</span>
-                        /xyz/qr.png
-                      </p>{" "}
-                      <div class="inline-flex items-center justify-center">
-                        <label class="relative flex items-center p-3 cursor-pointer">
-                          <input
-                            type="checkbox"
-                            checked={true}
-                            class="before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none border-2 border-gray-200 rounded-md transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-6 before:w-6 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-200 checked:bg-darkBlue checked:before:bg-gray-800 hover:before:opacity-10"
-                            id="check"
-                          />
-                          <span class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              class="h-4 w-4"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                              stroke="currentColor"
-                              stroke-width="1"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                clipRule="evenodd"
-                              ></path>
-                            </svg>
-                          </span>
-                        </label>
-                      </div>
-                    </div>
-                  );
+                  return <Row key={i} id={id} />;
                 })}
             </div>
           </div>
         </div>
+      </div>
+    </div>
+  );
+};
+
+const Row = ({ id }) => {
+  const [value, setValue] = useState(5);
+  const [showInput, setShowInput] = useState(false);
+
+  return (
+    <div
+      className="grid border border-gray-300/30 mb-3 pl-6 pr-4 py-1 rounded-lg items-center"
+      style={{ gridTemplateColumns: "5% 22% 21% 20% 20% 12%" }}
+    >
+      <p className="text-center">{id}</p>
+      <div className="flex items-center justify-center">
+        <Image
+          src="/qr.png"
+          className="w-6/12"
+          alt="QR code"
+          width={1000}
+          height={1000}
+        />
+      </div>
+      <div className="flex items-center justify-center">
+        {!showInput ? (
+          <p className="text-center relative text-xl font-medium">
+            {value}
+            <CiEdit
+              className="absolute -bottom-2 cursor-pointer -right-4 bg-darkBlue text-white p-1 rounded-full text-xl"
+              onClick={() => setShowInput(!showInput)}
+            />
+          </p>
+        ) : (
+          <div className="flex items-center justify-center">
+            <label htmlFor="qrValue" className="mr-3">
+              Value:
+            </label>
+            <div className="relative w-3/12">
+              <input
+                type="text"
+                id="qrValue"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                placeholder="Enter QR Code Value"
+                className="outline-none border border-gray-400 px-4 rounded-lg text-lg w-full"
+              />
+              <MdCheck
+                className="absolute bg-green-500 -right-2 text-white text-xl cursor-pointer p-0.5 -bottom-1 rounded-full"
+                onClick={() => setShowInput(!showInput)}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+      <p className="text-center">A-102-102</p>
+      <p className="px-4 text-lg text-center">
+        <span className="font-medium mr-1">Path:</span>
+        /xyz/qr.png
+      </p>{" "}
+      <div class="inline-flex items-center justify-center">
+        <label class="relative flex items-center p-3 cursor-pointer">
+          <input
+            type="checkbox"
+            checked={true}
+            class="before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none border-2 border-gray-200 rounded-md transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-6 before:w-6 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-200 checked:bg-darkBlue checked:before:bg-gray-800 hover:before:opacity-10"
+            id="check"
+          />
+          <span class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-4 w-4"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              stroke="currentColor"
+              stroke-width="1"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </span>
+        </label>
       </div>
     </div>
   );
